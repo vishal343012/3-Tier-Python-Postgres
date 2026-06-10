75b49552b5f48f251d12f4f8e21e1d0a20913482
vishal vishwakarma <119778329+vishal343012@users.noreply.github.com>
2026-06-10 15:57:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-import os
+ import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://root:root@localhost/my_database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'AZURE_POSTGRESQL_CONNECTIONSTRING'
+    ) or 'postgresql://root:root@localhost/my_database'
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
